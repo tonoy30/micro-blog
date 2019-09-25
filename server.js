8e3c2eaf908396cbc82005b66f1657280dbeba7c
tonoy <tonoy.sust@gmail.com>
2019-09-25 13:55:18 +0600

    web technology project in nodejs

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const users = require("./routes/api/users");
+const profiles = require("./routes/api/profiles");
+const posts = require("./routes/api/posts");
+
+const app = express();
+// DB config
+const db = require("./config/keys").mongoURI;
+// Connect to mongo
+mongoose
+  .connect(db)
+  .then(() => console.log("Connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello"));
+// Use routes
+app.use("/api/users", users);
+app.use("/api/profiles", profiles);
+app.use("/api/posts", posts);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
